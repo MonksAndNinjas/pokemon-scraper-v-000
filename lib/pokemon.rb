35a711632a65fd9ebe6b133724812f7711a154eb
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-11 17:35:39 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,8 +1,6 @@
 class Pokemon
   attr_accessor :id, :name, :type, :db
 
-  @@all = []
-
   def initialize(id:, name:, type:, db:)
     @id = id
     @name = name
@@ -10,12 +8,11 @@ class Pokemon
     @db = db
   end
 
-  def self.all
-    @@all
-  end
-
   def self.save(name, type, database_connection)
     database_connection.execute("INSERT INTO pokemon (name, type) VALUES (?, ?)", name, type)
+  end
+
+  def self.find
   end
 
 end

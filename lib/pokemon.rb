38e8547ec80ead89ec5368f054833abc88cd45f8
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-11 18:31:44 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -23,7 +23,7 @@ class Pokemon
     Pokemon.new(id: pokemon_id, name: pokemon_name, type: pokemon_type, db: pokemon_db)
   end
 
-  def alter_hp
+  def alter_hp(new_hp, database_connection)
   end
 
 end

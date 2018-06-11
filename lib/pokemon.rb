23748c3c154cd0a2ef7ee3ce47cc9d79692911c9
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-11 16:39:11 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,9 +1,6 @@
 class Pokemon
 
-  def initialize(name:, type:, db:)
-    @name = name
-    @type = type
-    @db = db
+  def initialize(id:, name:, type:, db:)
   end
 
   #def self.save(name, breed, age, database_connection)

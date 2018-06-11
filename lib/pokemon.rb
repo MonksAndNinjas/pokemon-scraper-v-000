89295e9386cfe38dd5722b7cfda80f42f86acca5
Joseph Jimenez <blackbeltdiaries@gmail.com>
2018-06-11 18:55:38 +0000

    automatically backed up by learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -20,13 +20,13 @@ class Pokemon
     pokemon_type = pokemon[2]
     pokemon_db = database_connection
     #binding.pry
-    Pokemon.new(id: pokemon_id, name: pokemon_name, type: pokemon_type, db: pokemon_db)
+    Pokemon.new(id: pokemon_id, name: pokemon_name, type: pokemon_type, hp: 60, db: pokemon_db)
   end
 
   def alter_hp(new_hp, database_connection)
     #pokemon = self.class.find(self.id, database_connection)
     #pokemon.hp = new_hp
-    database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
+    #database_connection.execute("UPDATE pokemon SET hp = ? WHERE id = ?", new_hp, self.id)
     #binding.pry
   end
 
